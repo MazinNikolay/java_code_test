8442bcceff1a606634e833de9573d35c11a67bda
kolia <koliamba_85@mail.ru>
2024-11-24 23:50:47 +0800

    Исходный коммит

---
+++ src/test/java/com/example/java_code_test/JavaCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_code_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
