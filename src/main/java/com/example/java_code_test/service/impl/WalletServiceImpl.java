297be742e9853b28dba8ba7133bbb7a4189cae1f
kolia <koliamba_85@mail.ru>
2024-11-26 22:03:34 +0800

    Added transactional annotation

--- src/main/java/com/example/java_code_test/service/impl/WalletServiceImpl.java
+++ src/main/java/com/example/java_code_test/service/impl/WalletServiceImpl.java
@@ -5,7 +5,10 @@ import com.example.java_code_test.entity.WalletEntity;
 import com.example.java_code_test.repository.WalletRepository;
 import com.example.java_code_test.service.WalletService;
 import com.example.java_code_test.utils.WalletMapping;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -14,11 +17,14 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class WalletServiceImpl implements WalletService {
+    private final Logger logger = LoggerFactory.getLogger(WalletServiceImpl.class);
     private final WalletRepository repository;
     private final WalletMapping mapping;
 
     @Override
+    @Transactional
     public void operate(WalletDto wallet) {
+        logger.info("was invoked operate method");
         WalletEntity entity = checkWalletExist(wallet.getWalletId());
         BigDecimal resultAmount = calculateAmount(wallet, entity);
         wallet.setAmount(resultAmount);
@@ -26,7 +32,9 @@ public class WalletServiceImpl implements WalletService {
     }
 
     @Override
+    @Transactional
     public BigDecimal calculateAmount(WalletDto wallet, WalletEntity entity) {
+        logger.info("was invoked calculateAmount method");
         switch (wallet.getOperationType()) {
             case DEPOSIT -> entity.setAmount(entity.getAmount().add(wallet.getAmount()));
             case WITHDRAW -> {
@@ -43,16 +51,19 @@ public class WalletServiceImpl implements WalletService {
 
     @Override
     public BigDecimal getAmount(UUID walletId) {
+        logger.info("was invoked getAmount method");
         return checkWalletExist(walletId).getAmount();
     }
 
     @Override
     public boolean checkAmountSize(BigDecimal initAmount, BigDecimal operateAmount) {
+        logger.info("was invoked checkAmountSize method");
         return initAmount.subtract(operateAmount).longValue() >= 0;
     }
 
     @Override
     public WalletEntity checkWalletExist(UUID walletId) {
+        logger.info("was invoked checkWalletExist method");
         return repository.findById(walletId).
                 orElseThrow(() -> new RuntimeException("Wallet is not found"));
     }
